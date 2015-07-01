f1c6caeb921f649c42feff74766d2dc5bd370029
Serhii Lysovenko <Lysovenko@users.noreply.github.com>
2015-07-01 14:09:49 +0300

    bones

---
+++ tickets.py
@@ -0,0 +1,71 @@
+# Copyright 2015 Serhiy Lysovenko
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"Making the face of the application"
+
+from tkinter import ttk, Text
+
+
+def autoscroll(sbar, first, last):
+    """Hide and show scrollbar as needed."""
+    first, last = float(first), float(last)
+    if first <= 0 and last >= 1:
+        sbar.grid_remove()
+    else:
+        sbar.grid()
+    sbar.set(first, last)
+
+
+class Tickets(ttk.Frame):
+    def __init__(self, parent, faco):
+        ttk.Frame.__init__(self, parent)
+        self.face_obj = faco
+        self.pw = pw = ttk.Panedwindow(self, orient="vertical")
+        pw.add(self.make_tree())
+        pw.add(self.make_text_field())
+        pw.grid(column=0, row=1, columnspan=2, sticky="senw")
+
+    def make_tree(self):
+        frame = ttk.Frame(self.pw)
+        frame.grid_columnconfigure(0, weight=1)
+        frame.grid_rowconfigure(0, weight=1)
+        self.tree = tree = ttk.Treeview(frame, selectmode="extended")
+        self.treef = frame
+        tree.grid(column=0, row=0, sticky="nwes")
+        vsb = ttk.Scrollbar(frame, command=tree.yview, orient="vertical")
+        vsb.grid(column=1, row=0, sticky="ns")
+        tree["yscrollcommand"] = lambda f, l: autoscroll(vsb, f, l)
+        hsb = ttk.Scrollbar(
+            frame, command=tree.xview, orient="horizontal")
+        hsb.grid(column=0, row=1, sticky="ew")
+        tree["xscrollcommand"] = lambda f, l: autoscroll(hsb, f, l)
+        tree.tag_configure("page", background="gray")
+        tree.tag_configure("file", foreground="blue", font="Monospace 12")
+        tree.tag_configure("bmk", foreground="red")
+        tree.tag_configure("folder", font="Times 14 bold")
+        return frame
+
+    def make_text_field(self):
+        frame = ttk.Frame(self.pw)
+        frame.grid_columnconfigure(0, weight=1)
+        frame.grid_rowconfigure(0, weight=1)
+        text = Text(frame, state="disabled", wrap="word",
+                    font="Times 14")
+        self.text = text
+        text.grid(column=0, row=0, sticky="nwes")
+        vsb = ttk.Scrollbar(frame, command=self.text.yview, orient="vertical")
+        vsb.grid(column=1, row=0, sticky="ns")
+        text["yscrollcommand"] = lambda f, l: autoscroll(vsb, f, l)
+        self.text_curinfo = None
+        text.tag_configure("h1", font="Times 16 bold", relief="raised")
+        return frame        
