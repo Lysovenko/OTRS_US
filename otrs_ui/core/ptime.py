c0178713918cebb4af16fa0d0978c987aa28fd8a
Serhii Lysovenko <Lysovenko@users.noreply.github.com>
2016-01-14 23:48:39 +0200

    update copyright years

--- otrs_ui/core/ptime.py
+++ otrs_ui/core/ptime.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright 2015 Serhiy Lysovenko
+# Copyright 2015-2016 Serhiy Lysovenko
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
--- otrs_ui/gui/dashboard.py
+++ otrs_ui/gui/dashboard.py
@@ -1,4 +1,4 @@
-# Copyright 2015 Serhiy Lysovenko
+# Copyright 2015-2016 Serhiy Lysovenko
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
