60fe42c379bca1f1c5a3914a97b8ed0612e785d6
Serhii Lysovenko <Lysovenko@users.noreply.github.com>
2016-02-28 00:02:20 +0200

    replace an empty file

--- otrs_ui/core/parse/messages.py
+++ otrs_ui/core/parse/messages.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 "parse tickets page"
-from .basic import BasicParser
+from . import BasicParser
 
 
 class MessageParser(BasicParser):
