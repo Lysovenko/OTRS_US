67eae74206e41cea6f07a0f097e77fa40c160ff7
Serhii Lysovenko <Lysovenko@users.noreply.github.com>
2016-05-06 18:18:56 +0300

    got 3 pannels

--- otrs_us/curses/__init__.py
+++ otrs_us/curses/__init__.py
@@ -13,59 +13,27 @@
 # limitations under the License.
 "Making the cue of the application"
 
-from curses import wrapper, ALL_MOUSE_EVENTS, KEY_MOUSE, mousemask, getmouse
+from curses import (
+    wrapper, ALL_MOUSE_EVENTS, KEY_MOUSE, mousemask, getmouse, echo, A_BOLD,
+    newwin, doupdate)
+from curses.panel import new_panel, update_panels
 from curses.textpad import Textbox, rectangle
 
 
-def get_param(prompt_string, screen):
-    screen.clear()
-    screen.border(0)
-    screen.addstr(2, 2, prompt_string)
-    screen.refresh()
-    istr = screen.getstr(10, 10, 60)
-    return str(istr)
-
-
-def execute_cmd(cmd_string):
-    # system("clear")
-    a = 0
-    print("")
-    if a == 0:
-        print("Command executed correctly")
-    else:
-        print("Command terminated with error")
-    # input("Press enter")
-    print("")
-
-
 def main(scr):
-    x = 0
-    while x != ord('4'):
-#        scr = curses.initscr()
-        scr.clear()
-        scr.border(0)
-        scr.addstr(2, 2, "Please enter a number...")
-        scr.addstr(4, 4, "1 - Add a user")
-        scr.addstr(5, 4, "2 - Restart Apache")
-        scr.addstr(6, 4, "3 - Show disk space")
-        scr.addstr(7, 4, "4 - Exit")
-        scr.refresh()
-        x = scr.getch()
-        if x == ord('1'):
-            username = get_param("Enter the username", scr)
-            homedir = get_param("Enter the home directory, eg /home/nate", scr)
-            groups = get_param(
-                "Enter comma-separated groups, eg adm,dialout,cdrom", scr)
-            shell = get_param("Enter the shell, eg /bin/bash:", scr)
-            # curses.endwin()
-            execute_cmd("useradd -d " + homedir + " -g 1000 -G " + groups +
-                        " -m -s " + shell + " " + username)
-        if x == ord('2'):
-            # curses.endwin()
-            execute_cmd("df")
-        if x == ord('3'):
-            # curses.endwin()
-            execute_cmd("df -h")
+    h, w = scr.getmaxyx()
+    hghts = [h // 3 + (1 if 3 - h % 3 <= i else 0) for i in range(3)]
+    pr = 0
+    wins = []
+    for i in hghts:
+        wins.append(newwin(i, w, pr, 0))
+        pr += i
+    for i in range(3):
+        wins[i].box(0, 0)
+    panels = [new_panel(i) for i in wins]
+    update_panels()
+    doupdate()
+    scr.getch()
 
 
 def start_cue():
